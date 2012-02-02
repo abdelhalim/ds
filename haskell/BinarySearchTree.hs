4f89bdc858ad49cce392274ffc10d6710b1c6160
Abdelhalim Ragab <abdelhalim@r8t.org>
2012-02-01 20:21:54 -0800

    add treeSize and couple of simple examples

--- haskell/BinarySearchTree.hs
+++ haskell/BinarySearchTree.hs
@@ -1,3 +1,12 @@
 
 -- Type for Binary Search Tree
 data Tree a = Leaf | Node a (Tree a) (Tree a)
+
+treeSize Leaf = 0
+treeSize (Node a left right) = 1 + treeSize left + treeSize right
+
+
+-- Example
+
+tree1 = Node 10 Leaf Leaf
+tree2 = Node 10 (Node 5 Leaf Leaf) (Node 12 Leaf Leaf)
